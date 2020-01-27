5e9ea61eaa33722268dda010341694db7adc1ca8
Lauren Bannon <laurenbannon98@gmail.com>
2020-01-27 22:00:32 +0000

    Fixed text extraction from all office files

--- SenseEar-Project/SenseEar-Project/Controller/ViewController+FileManagement.swift
+++ SenseEar-Project/SenseEar-Project/Controller/ViewController+FileManagement.swift
@@ -59,7 +59,7 @@ extension ViewController: UIDocumentPickerDelegate {
                        print("Text File Not Found")
                    }
                     
-                } else if selectedFile!.pathExtension == "docx" {
+                } else if selectedFile!.pathExtension == "docx" || selectedFile!.pathExtension == "xlsx" || selectedFile!.pathExtension == "pptx" {
                                         
                     do {
                             PTConvert.convertOffice(toPDF: filePath!, paperSize: .zero) { (pathToPDF) in
@@ -71,68 +71,38 @@ extension ViewController: UIDocumentPickerDelegate {
                                 let urlToPDF = URL(fileURLWithPath: pathToPDF)
                                 let destinationURL = URL(fileURLWithPath: filePathWithoutFilename!).appendingPathComponent(urlToPDF.lastPathComponent)
                                 
+                                print("HERE \(filePathWithoutFilename)")
+                                print(urlToPDF.lastPathComponent)
+                                
                                 self.newConvertedPdf = urlToPDF
                                     
                                 do {
                                     
-//                                    if let pdf = PDFDocument(url: self.newConvertedPdf!) {
-//                                        let pageCount = pdf.pageCount
-//                                        let documentContent = NSMutableAttributedString()
-//                                        var content = String()
-//
-//                                        for i in 1 ..< pageCount {
-//                                            guard let page = pdf.page(at: i) else { continue }
-//                                            guard let pageContent = page.string else { continue }
-//
-//                                            content = pageContent
-////                                            guard let pageContent = page.attributedString else { continue }
-////                                            documentContent.append(pageContent)
-////                                            content = documentContent.mutableString as String
-////                                            content = pageContent.string
-//                                        }
-//
-////                                         print("NEW PDF CONTENT: \(content)")
-////
-////                                        for i in 1 ..< pageCount {
-////                                            guard let page = pdf.page(at: i) else { continue }
-////                                            guard let pageContent = page.attributedString else { continue }
-////                                            documentContent.append(pageContent)
-////
-////
-////
-////                                            print("NEW PDF CONTENT: \(documentContent)")
-////                                        }
-//                                    }
-                                    
-//                                    var pdfContents: String = ""
-//
-//                                    if let page = PDFDocument(url: self.newConvertedPdf!) {
-//                                        pdfContents = page.string!
-//
-//                                        print("PDF File!")
-//                                        print(pdfContents)
-//
-//                                    }
-                                    
+                                    var pdfContents: String = ""
+
+                                    if let page = PDFDocument(url: self.newConvertedPdf!) {
+                                        pdfContents = page.string!
+
+                                        print("PDF File!")
+                                        print(pdfContents)
+
+                                    }
                                     
                                 } catch {
                                     print("YOU SUCK! \(error)")
                                 }
                             
                         }
-                        
 
                     } catch {
                         print("Word Document File Not Found")
                     }
                     
-                    print("Word Document!")
-                    
                 } else if selectedFile!.pathExtension == "pdf" {
                     
                     extractTextFromPDF()
                     
-                }
+                } 
                     
            } else {
                print("File does not exist!")
@@ -167,43 +137,4 @@ extension ViewController: UIDocumentPickerDelegate {
         return pdfContents
     }
     
-    func simpleConvert(inputPath: String, outputPath: String) {
-           // Start with a PDFDoc (the conversion destination)
-           let pdfDoc: PTPDFDoc = PTPDFDoc()
-
-           // perform the conversion with no optional parameters
-           PTConvert.office(toPDF: pdfDoc, in_filename: inputPath, options: nil)
-
-           pdfDoc.save(toFile: outputPath, flags: e_ptremove_unused.rawValue)
-
-           print("Saved: \(outputPath)")
-    }
-
-}
-
-extension NSAttributedString {
-     public func attributedStringByTrimmingCharacterSet(charSet: NSCharacterSet) -> NSAttributedString {
-         let modifiedString = NSMutableAttributedString(attributedString: self)
-        modifiedString.trimCharactersInSet(charSet: charSet)
-         return NSAttributedString(attributedString: modifiedString)
-     }
-}
-
-extension NSMutableAttributedString {
-     public func trimCharactersInSet(charSet: NSCharacterSet) {
-        var range = (string as NSString).rangeOfCharacter(from: charSet as CharacterSet)
-
-         // Trim leading characters from character set.
-         while range.length != 0 && range.location == 0 {
-            replaceCharacters(in: range, with: "")
-            range = (string as NSString).rangeOfCharacter(from: charSet as CharacterSet)
-         }
-
-         // Trim trailing characters from character set.
-        range = (string as NSString).rangeOfCharacter(from: charSet as CharacterSet, options: .backwards)
-         while range.length != 0 && NSMaxRange(range) == length {
-            replaceCharacters(in: range, with: "")
-            range = (string as NSString).rangeOfCharacter(from: charSet as CharacterSet, options: .backwards)
-         }
-     }
 }
