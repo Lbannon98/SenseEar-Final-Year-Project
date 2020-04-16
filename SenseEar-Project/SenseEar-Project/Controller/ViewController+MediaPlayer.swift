5cf45a827ed626846369f58555e463dc0ad6583f
Lauren Bannon <laurenbannon98@gmail.com>
2020-04-16 12:37:36 +0100

    Attempt at adding player

--- SenseEar-Project/SenseEar-Project/Controller/ViewController+MediaPlayer.swift
+++ SenseEar-Project/SenseEar-Project/Controller/ViewController+MediaPlayer.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import MediaPlayer
+
+extension ViewController {
+    
+    func setupMediaPlayerNotificationView() {
+        
+        let commandCenter = MPRemoteCommandCenter.shared()
+        
+        //Add handler for Play Command
+        commandCenter.playCommand.addTarget { [unowned self] event in
+            
+            self.player!.play()
+            return .success
+        
+        }
+        
+        // Add handler for Pause Command
+        commandCenter.pauseCommand.addTarget { [unowned self] event in
+            
+            self.player!.pause()
+            return .success
+        
+        }
+        
+    }
+    
+    func setupNotificationView() {
+        
+        nowPlayingInfo = [String : Any]()
+        
+        nowPlayingInfo![MPMediaItemPropertyTitle] = filename
+        
+        nowPlayingInfo![MPNowPlayingInfoPropertyPlaybackRate] = 1
+        
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+        
+        
+    }
+    
+}
